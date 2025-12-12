04cd73496bfd427ab8f8c2d794b22c98441796da
Danechka-q <gargon38rus1@gmail.com>
2025-12-12 13:58:43 +0500

    v.0.2.2

--- src/data/events.all.data.ts
+++ src/data/events.all.data.ts
@@ -12,44 +12,9 @@ export type PropsEvent = {
 }
 
 export const EVENTSINFOALL: PropsEvent[] = [
+
     {
         id: "1",
-        img: '/imagesForAfisha/rad-04.jpg',
-        title: 'Родион Коев',
-        description: 'Вечер утонченной фортепианной музыки. Изысканные мелодии в исполнении Родиона Коева',
-        price: "≈ free",
-        date: '04.12.2025',
-        dataTcEvent: "",
-        dataTcToken: "",
-        schema: false,
-        prewDate: '19:00 04.12.2025'
-    },
-    {
-        id: "2",
-        img: '/imagesForAfisha/inna-05.jpg',
-        title: 'Инна Иртюго',
-        description: 'На сцене кабаре - Инна Иртюго с незабываемым вокальным выступлением',
-        price: "≈ free",
-        date: '05.12.2025',
-        dataTcEvent: "",
-        dataTcToken: "",
-        schema: false,
-        prewDate: '19:00 05.12.2025'
-    },
-    {
-        id: "3",
-        img: '/imagesForAfisha/ilya-06.jpg',
-        title: 'Илья Гаврюсев',
-        description: 'В этот вечер кабаре Илья Гаврюсев с сольной программой на саксофоне',
-        price: "≈ free",
-        date: '06.12.2025',
-        dataTcEvent: "",
-        dataTcToken: "",
-        schema: false,
-        prewDate: '20:00 06.12.2025'
-    },
-    {
-        id: "4",
         img: '/imagesForAfisha/ilya-12.jpg',
         title: 'Илья Гаврюсев',
         description: 'Вновь на сцене кабаре Илья Гаврюсев с сольной программой на саксофоне ',
@@ -61,7 +26,7 @@ export const EVENTSINFOALL: PropsEvent[] = [
         prewDate: '19:00 12.12.2025'
     },
     {
-        id: "5",
+        id: "2",
         img: '/imagesForAfisha/rad-13.jpg',
         title: 'Родион Коев и Инна Иртюго',
         description: 'Сцена кабаре представляет музыкальный альянс фортепиано и вокала в исполнении Родиона Коева и Инны Иртюго',
@@ -72,32 +37,81 @@ export const EVENTSINFOALL: PropsEvent[] = [
         schema: false,
         prewDate: '20:00 13.12.2025'
     },
-     {
-        id: "6",
+    {
+        id: "3",
         img: '/imagesForAfisha/corp1.jpg',
         title: 'Корпоративные вечера в стиле голливудской сказки',
-        description: 'Каждый момент вечера продуман до мелочей: от приветственного бокала до финальных аплодисментов',
+        description: 'Welcome drink, фуршет и шоу программа!',
         price: "≈ 9800",
-        date: '18/19/20.12.2025',
+        date: '18.12.2025',
         dataTcEvent: "",
         dataTcToken: "",
         schema: false,
-        prewDate: '18/19/20.12.2025'
+        prewDate: '18.12.2025 19:00'
+    },
+    {
+        id: "4",
+        img: '/imagesForAfisha/corp1.jpg',
+        title: 'Корпоративные вечера в стиле голливудской сказки',
+        description: 'Welcome drink, фуршет и шоу программа!',
+        price: "≈ 3900",
+        date: '19.12.2025',
+        dataTcEvent: "693bcc8ed6f625b2db3df202",
+        dataTcToken: "eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjhkYjkxMjc3ZDk2N2IyZWRkYWIzOWVjIn0.x53ioRnq2WdVMPetZaGGXsvm74ZWDn0xYumYpfGofL0",
+        schema: true,
+        prewDate: '19.12.2025 19:00'
+    },
+    {
+        id: "5",
+        img: '/imagesForAfisha/corp1.jpg',
+        title: 'Корпоративные вечера в стиле голливудской сказки',
+        description: 'Welcome drink, фуршет и шоу программа!',
+        price: "≈ 3900",
+        date: '20.12.2025',
+        dataTcEvent: "693bd77e6da6c412691d9b0e",
+        dataTcToken: "eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjhkYjkxMjc3ZDk2N2IyZWRkYWIzOWVjIn0.x53ioRnq2WdVMPetZaGGXsvm74ZWDn0xYumYpfGofL0",
+        schema: true,
+        prewDate: '20.12.2025 19:00'
+    },
+    {
+        id: "6",
+        img: '/imagesForAfisha/corp1.jpg',
+        title: 'Корпоративные вечера в стиле голливудской сказки',
+        description: 'Welcome drink, фуршет и шоу программа!',
+        price: "≈ 3900",
+        date: '25.12.2025',
+        dataTcEvent: "",
+        dataTcToken: "",
+        schema: false,
+        prewDate: '25.12.2025 19:00'
     },
     {
         id: "7",
         img: '/imagesForAfisha/corp1.jpg',
         title: 'Корпоративные вечера в стиле голливудской сказки',
-        description: 'Изысканное шеф-меню, шоу программа и welcome-зона',
-        price: "≈ 9800",
-        date: '25/26/27.12.2025',
-        dataTcEvent: "",
-        dataTcToken: "",
-        schema: false,
-        prewDate: '25/26/27.12.2025'
+        description: 'Welcome drink, фуршет и шоу программа!',
+        price: "≈ 3900",
+        date: '26.12.2025',
+        dataTcEvent: "693bd8233c0bc02c020df0ab",
+        dataTcToken: "eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjhkYjkxMjc3ZDk2N2IyZWRkYWIzOWVjIn0.x53ioRnq2WdVMPetZaGGXsvm74ZWDn0xYumYpfGofL0",
+        schema: true,
+        prewDate: '26.12.2025 19:00'
     },
     {
         id: "8",
+        img: '/imagesForAfisha/corp1.jpg',
+        title: 'Корпоративные вечера в стиле голливудской сказки',
+        description: 'Welcome drink, фуршет и шоу программа!',
+        price: "≈ 3900",
+        date: '27.12.2025',
+        dataTcEvent: "693bd85d63644d6c6975b649",
+        dataTcToken: "eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjhkYjkxMjc3ZDk2N2IyZWRkYWIzOWVjIn0.x53ioRnq2WdVMPetZaGGXsvm74ZWDn0xYumYpfGofL0",
+        schema: true,
+        prewDate: '27.12.2025 19:00'
+    },
+    
+    {
+        id: "9",
         img: '/imagesForAfisha/ng.jpg',
         title: 'Новогодняя ночь в стиле Золотой Голливуд',
         description: 'Иллюзионист, танцевальные номера и живой вокал',
@@ -109,7 +123,7 @@ export const EVENTSINFOALL: PropsEvent[] = [
         prewDate: '31.12.2025'
     },
     {
-        id: "9",
+        id: "10",
         img: '/imagesForAfisha/10.01.jpg',
         title: 'Гастро STANDUP "Жарю Медиум"',
         description: 'Новый формат ужина от Сергея Малаховского, где сочный стейк и острый юмор подаются одновременно',
