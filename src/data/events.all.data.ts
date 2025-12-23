0ca3aecd5775a0a7454f97587fb625d9f070b1d3
Danechka-q <gargon38rus1@gmail.com>
2025-12-23 07:03:24 +0500

    v.0.2.6

--- src/data/events.all.data.ts
+++ src/data/events.all.data.ts
@@ -14,31 +14,7 @@ export type PropsEvent = {
 export const EVENTSINFOALL: PropsEvent[] = [
 
 
-    
-    {
-        id: "7",
-        img: '/imagesForAfisha/corp1.jpg',
-        title: 'Корпоративные вечера в стиле голливудской сказки',
-        description: 'Welcome drink, фуршет и шоу программа!',
-        price: "≈ 3900",
-        date: '26.12.2025',
-        dataTcEvent: "693bd8233c0bc02c020df0ab",
-        dataTcToken: "eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjhkYjkxMjc3ZDk2N2IyZWRkYWIzOWVjIn0.x53ioRnq2WdVMPetZaGGXsvm74ZWDn0xYumYpfGofL0",
-        schema: true,
-        prewDate: '26.12.2025 19:00'
-    },
-    {
-        id: "8",
-        img: '/imagesForAfisha/corp1.jpg',
-        title: 'Корпоративные вечера в стиле голливудской сказки',
-        description: 'Welcome drink, фуршет и шоу программа!',
-        price: "≈ 3900",
-        date: '27.12.2025',
-        dataTcEvent: "693bd85d63644d6c6975b649",
-        dataTcToken: "eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjhkYjkxMjc3ZDk2N2IyZWRkYWIzOWVjIn0.x53ioRnq2WdVMPetZaGGXsvm74ZWDn0xYumYpfGofL0",
-        schema: true,
-        prewDate: '27.12.2025 19:00'
-    },
+
     
     {
         id: "9",
